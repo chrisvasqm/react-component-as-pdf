a1d8b905ce2e85c9ac5e739184913bac35f4283d
chrisvasqm <christianvasquezmelo@gmail.com>
2024-08-11 18:08:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Components to PDF
+
+Demostration project to show how to create PDF files out of React Components.
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Components to PDF</h1>
+    </>
+  )
+}
+
+export default App
