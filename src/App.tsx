bf20fd3fd8b0c4bdbcff75c208210637e0d4b54a
chrisvasqm <christianvasquezmelo@gmail.com>
2024-08-11 18:37:02 -0400

    Add preview and download PDF button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import './App.css'
-import RenderMe from './components/RenderMe'
+import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
+import './App.css';
+import RenderMe from './components/RenderMe';
 
 function App() {
 
   return (
-    <>
-      <h1>Components to PDF</h1>
-      <RenderMe />
-    </>
+    <div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
+        <h1>Components to PDF</h1>
+        <PDFDownloadLink
+          document={<RenderMe />}
+          fileName={`example-${new Date().toLocaleDateString()}.pdf`}
+          style={{
+            textDecoration: 'none',
+            padding: '10px 20px',
+            color: '#fff',
+            backgroundColor: '#007bff',
+            border: 'none',
+            borderRadius: '4px',
+            cursor: 'pointer',
+            maxWidth: '100px'
+          }}
+        >{({ loading }) => loading ? 'Generating PDF...' : 'Download PDF'}</PDFDownloadLink>
+          <PDFViewer height={'500px'}>
+            <RenderMe />
+          </PDFViewer>
+      </div>
+    </div>
   )
 }
 
--- src/components/RenderMe.tsx
+++ src/components/RenderMe.tsx
@@ -1,28 +1,14 @@
+import { Document, Page, View, Text } from '@react-pdf/renderer';
 
 const RenderMe = () => {
   return (
-    <>
-      <h2 style={{ color: 'dodgerblue' }}>Invoice #123</h2>
-      <p style={{ color: 'tomato' }}>Description of the sale</p>
-      <p>{new Date().toLocaleDateString()}</p>
-
-      <table style={{ border: '1px solid black' }}>
-        <thead>
-          <th>Item</th>
-          <th>Price</th>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Milk</td>
-            <td>$9.99</td>
-          </tr>
-          <tr>
-            <td>Eggs</td>
-            <td>$4.99</td>
-          </tr>
-        </tbody>
-      </table>
-    </>
+    <Document>
+      <Page size="A4" style={{ flexDirection: 'row', backgroundColor: '#E4E4E4' }}>
+        <View style={{ margin: 10, padding: 10, flexGrow: 1 }}>
+          <Text style={{fontSize: 40}}>Invoice</Text>
+        </View>
+      </Page>
+    </Document>
   )
 }
 
